76852d61cbc2efd6b7aebb0727aa534c110d95e0
mtaylo12 <70495835+mtaylo12@users.noreply.github.com>
2023-04-25 09:21:52 -0400

    first commit for lassen-cpu additions

---
+++ components/eamxx/cmake/machine-files/lassen-cpu.cmake
@@ -0,0 +1,21 @@
+include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+common_setup()
+
+# set up Kokkos
+include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
+
+
+# set up MPI (done in included file?)
+include (${EKAT_MACH_FILES_PATH}/mpi/other.cmake)
+
+set(EKAT_MPIRUN_EXE "jsrun -E LD_PRELOAD=/opt/ibm/spectrum_mpi/lib/pami_471/libpami.so" CACHE STRING "" FORCE)
+
+set(EKAT_MPI_NP_FLAG "--np" CACHE STRING "")
+
+# set up TPLs
+#set(NetCDF_Fortran_PATH /usr/gdata/climdat/libs/netcdf-fortran/install/lassen/fortran CACHE STRING "")
+set(BLAS_LIBRARIES /usr/gdata/climdat/libs/blas/libblas.a CACHE STRING "")
+set(LAPACK_LIBRARIES /usr/gdata/climdat/libs/lapack/liblapack.a CACHE STRING "")
+
+# set up input root
+set(SCREAM_INPUT_ROOT "/usr/gdata/climdat/ccsm3data/inputdata/" CACHE STRING "")
